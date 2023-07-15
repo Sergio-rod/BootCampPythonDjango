6be0b6077c5d3a221900e33a9790929d67daf669
Sergio Rodriguez Hernandez <rodriguezhernandezsdc@gmail.com>
2023-07-14 22:40:49 -0600

    day16

--- CoffeMachine/main.py
+++ CoffeMachine/main.py
@@ -34,13 +34,32 @@ resources = {
 # TODO: 1. Print the report of all coffee machine resources
 
 
-#handling variables
+#HANDLING VARIABLES
+
+#EXPRESSO QTY OF ITEMS REQUIRED
 espressoWaterQty = MENU['espresso']['ingredients']['water']
 espressoCoffeQty= MENU['espresso']['ingredients']['coffe']
+espressoCost = MENU['espresso']['cost']
+
+#LATTE QTY OF ITEMS REQUIRED
+latteWaterQty = MENU['latte']['ingredients']['water']
+latteCoffeQty= MENU['latte']['ingredients']['coffe']
+latteMilkQty= MENU['latte']['ingredients']['milk']
+latteCost = MENU['latte']['cost']
+
+
+#EXPRESSO QTY OF ITEMS REQUIRED
+capuccinoWaterQty = MENU['capuccino']['ingredients']['water']
+capuccinoCoffeQty = MENU['capuccino']['ingredients']['coffe'] 
+capuccinoMilkQty = MENU['capuccino']['ingredients']['milk']
+capucchinoCost = MENU['capuccino']['cost']
 
 
 def menu():
     money = 0
+    change= 0
+    
+    
     option = input('What would you like? (espresso/latte/capuccino): \n')
     if option == 'report':
         print(resources)
@@ -57,14 +76,35 @@ def menu():
                     print('That`s not possible')
 
                 dimes = input('Please, insert the dimes')
+                if dimes > 0:
+                    money += dimes*.25
+                elif dimes < 0:
+                    print('That`s not possible')
+                
                 nickles = input('Please, insert the nickles')
+                if nickles > 0:
+                    money += nickles*.25
+                elif nickles < 0:
+                    print('That`s not possible')
+                
                 pennies = input('Please, insert the pennies')
+                if pennies > 0:
+                    money += pennies*.25
+                elif pennies < 0:
+                    print('That`s not possible')
             except ValueError:
                 print('You must to enter a valid values ')
-
-            
+                
+            if not(money<capucchinoCost):
+                change = money-capucchinoCost
+                print(f"Take your coffe, your change is {change}")
+            else:
+                print('You need more money')
+                exit()     
         else: 
             print('Sorry, its not enough ingredients')
+            exit()
+            
 
 
 
