a258d6a60b17769a4c3bd163f183b2dbbca3bf32
Sergio Rodriguez Hernandez <rodriguezhernandezsdc@gmail.com>
2023-07-22 21:45:55 -0600

    Day20-Snake_Game_Complete

--- day20_Snake_Game/main.py
+++ day20_Snake_Game/main.py
@@ -29,14 +29,22 @@ while game_is_on:
     screen.update()
     snake.move()
 
-    # Detect colition with food
+    # Detect coalition with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         score.increment_points()
-    # Dtect colition with corners
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    # Detect coalition with corners
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         score.game_over()
+    # Detect coalition with itself
+    for dot in snake.snake[1:]:
+        if snake.head.distance(dot) < 10:
+            game_is_on = False
+            score.game_over()
+
+
 
 
 
---
+++ day20_Snake_Game/snake.py
@@ -0,0 +1,55 @@
+from turtle import Turtle
+import time
+
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+STARTING_POSITIONS = ((0,0),(-20,0),(-40,0))
+
+
+class Snake:
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def add_dot(self, position):
+        new_dot = Turtle('square')
+        new_dot.color('green')
+        new_dot.penup()
+        new_dot.goto(position)
+        self.snake.append(new_dot)
+
+    def extend(self):
+        self.add_dot(self.snake[-1].position())
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_dot(position)
+
+    def move(self):
+        time.sleep(.1)
+        for snake_dot in range(len(self.snake) - 1, 0, -1):
+            new_x = self.snake[snake_dot - 1].xcor()
+            new_y = self.snake[snake_dot - 1].ycor()
+            self.snake[snake_dot].goto(new_x, new_y)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
+
