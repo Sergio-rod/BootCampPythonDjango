5e724ab8b42efc41385cd9dd79a65460246f1c8f
Sergio Rodriguez Hernandez <rodriguezhernandezsdc@gmail.com>
2023-07-26 22:31:37 -0600

    day21-complete

--- day21_Pong_Game/main.py
+++ day21_Pong_Game/main.py
@@ -4,8 +4,8 @@ from paddle import Paddle
 from score import ScoreBoard
 from ball import Ball
 
-POSITIONS1 = [(-280, 40), (-280, 20), (-280, 0), (-280, -20), (-280, -40)]
-POSITIONS2 = [(280, 40), (280, 20), (280, 0), (280, -20), (280, -40)]
+POSITION1 = (-280, 0)
+POSITION2 = (280, 0)
 
 
 
@@ -34,8 +34,8 @@ for i in range(0, 30 ,2):
 # ---------
 
 
-paddle = Paddle(POSITIONS1)
-paddle1 = Paddle(POSITIONS2)
+paddle = Paddle(POSITION1)
+paddle1 = Paddle(POSITION2)
 score = ScoreBoard()
 ball = Ball()
 
@@ -45,8 +45,14 @@ ball = Ball()
 
 
 screen.listen()
+
 screen.onkeypress(paddle.move_up, 'w')
 screen.onkeypress(paddle.move_down, 's')
+
+"""
+screen.onkeypress(paddle1.move_up, '-')
+screen.onkeypress(paddle1.move_down, '+')
+"""
 
 
 
@@ -57,42 +63,43 @@ game_on = True
 banner = True
 
 while game_on:
-    time.sleep(0.1)
+    time.sleep(.1)
     screen.update()
     ball.move()
 
 
-
-
-
-
-
+    # AutomaticPlay
     if banner:
         paddle1.move_up()
-        if not(paddle1.highest_segment.ycor() < 280):
+        if not(paddle1.position.ycor() < 240):
             banner = False
     elif not banner:
         paddle1.move_down()
-        if not(paddle1.lowest_segment.ycor() > -280):
+        if not(paddle1.position.ycor() > -240):
             banner = True
 
     # Detect colision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
-    if ball.xcor() > 280 or ball.xcor() < -280:
-        ball.bounce_x()
+
 
     # Detect colision with left paddle
-    for segment in paddle.segments:
-        if ball.distance(segment) < 30:
-            ball.bounce_x()
-            score.increment_left_score()
+
+    if ball.distance(paddle.position) < 30:
+        ball.bounce_x()
+        score.increment_left_score()
 
     # Detect colision with right paddle
-    for segment in paddle1.segments:
-        if ball.distance(segment) < 30:
-            ball.bounce_x()
-            score.increment_right_score()
+    if ball.distance(paddle1.position) < 30:
+        ball.bounce_x()
+        score.increment_right_score()
+
+    # Detect right miss
+    if ball.xcor() > 280:
+        ball.reset_position()
+        # Detect right miss
+    if ball.xcor() < -280:
+        ball.reset_position()
 
 
 
--- day21_Pong_Game/paddle.py
+++ day21_Pong_Game/paddle.py
@@ -7,47 +7,34 @@ import time
 
 UP = 90
 DOWN = 270
-RIGHT = 0
-LEFT = 180
+
 
 
 class Paddle:
-    def __init__(self, positions):
-        self.segments = []
-        self.create_paddle(self.segments, positions)
-
-        self.highest_segment = self.segments[0]
-        self.lowest_segment = self.segments[len(self.segments)-1]
+    def __init__(self, position):
+        self.position = self.create_paddle(position)
 
 
-    def create_paddle(self, segments, positions):
-        for position in positions:
-            new_segment = Turtle('square')
-            new_segment.speed('fastest')
-            new_segment.penup()
-            new_segment.goto(position)
-            new_segment.color('white')
-            segments.append(new_segment)
+
+    def create_paddle(self, position):
+        paddle = Turtle('square')
+        paddle.speed('fastest')
+        paddle.turtlesize(1, 6)
+        paddle.seth(UP)
+        paddle.penup()
+        paddle.goto(position)
+        paddle.color('white')
+        return paddle
 
     def move_up(self):
-        self.highest_segment.seth(UP)
-        self.up()
-        self.highest_segment.forward(20)
+        if self.position.ycor() < 240:
+            self.position.seth(UP)
+            self.position.forward(20)
 
     def move_down(self):
-        self.lowest_segment.seth(DOWN)
-        self.down()
-        self.lowest_segment.forward(20)
-
-    def up(self):
-        for segment in range(len(self.segments)-1, 0, -1):
-            new_y = self.segments[segment - 1].ycor()
-            self.segments[segment].sety(new_y)
-
-    def down(self):
-        for segment in range(0, len(self.segments)-1):
-            new_y = self.segments[segment + 1].ycor()
-            self.segments[segment].sety(new_y)
+        if self.position.ycor() > -240:
+            self.position.seth(DOWN)
+            self.position.forward(20)
 
 
 
