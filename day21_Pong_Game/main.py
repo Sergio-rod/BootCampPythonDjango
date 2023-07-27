bfdf65604461c4b58fc745a703339f9462ad7ee7
Sergio Rodriguez Hernandez <rodriguezhernandezsdc@gmail.com>
2023-07-26 21:35:41 -0600

    Fixing bugs

--- day21_Pong_Game/main.py
+++ day21_Pong_Game/main.py
@@ -1,6 +1,14 @@
+import time
 from turtle import Screen, Turtle
 from paddle import Paddle
 from score import ScoreBoard
+from ball import Ball
+
+POSITIONS1 = [(-280, 40), (-280, 20), (-280, 0), (-280, -20), (-280, -40)]
+POSITIONS2 = [(280, 40), (280, 20), (280, 0), (280, -20), (280, -40)]
+
+
+
 
 
 screen = Screen()
@@ -12,7 +20,7 @@ screen.tracer(0)
 # CREATE BOARD
 
 startPosition = 280
-for i in range(0,30,2):
+for i in range(0, 30 ,2):
     # CREATE BOARD
     dot_line = Turtle('square')
     dot_line.turtlesize(stretch_len=.2)
@@ -26,25 +34,67 @@ for i in range(0,30,2):
 # ---------
 
 
-player1 = Paddle()
+paddle = Paddle(POSITIONS1)
+paddle1 = Paddle(POSITIONS2)
 score = ScoreBoard()
+ball = Ball()
 
 
-
-
+# paddle 2
 
 
 
 screen.listen()
-screen.onkeypress(player1.move_up, 'w')
-screen.onkeypress(player1.move_down, 's')
+screen.onkeypress(paddle.move_up, 'w')
+screen.onkeypress(paddle.move_down, 's')
+
+
+
 
 
 
 game_on = True
+banner = True
 
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+
+
+
+
+
+
+    if banner:
+        paddle1.move_up()
+        if not(paddle1.highest_segment.ycor() < 280):
+            banner = False
+    elif not banner:
+        paddle1.move_down()
+        if not(paddle1.lowest_segment.ycor() > -280):
+            banner = True
+
+    # Detect colision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.xcor() > 280 or ball.xcor() < -280:
+        ball.bounce_x()
+
+    # Detect colision with left paddle
+    for segment in paddle.segments:
+        if ball.distance(segment) < 30:
+            ball.bounce_x()
+            score.increment_left_score()
+
+    # Detect colision with right paddle
+    for segment in paddle1.segments:
+        if ball.distance(segment) < 30:
+            ball.bounce_x()
+            score.increment_right_score()
+
+
 
 
 
@@ -52,3 +102,7 @@ while game_on:
 
 
 screen.exitonclick()
+
+
+
+
--- day21_Pong_Game/paddle.py
+++ day21_Pong_Game/paddle.py
@@ -1,9 +1,8 @@
-from turtle import  Turtle
+from turtle import Turtle
 import time
 
 
-POSITIONS1 = [(-280, 40), (-280, 20), (-280, 0), (-280, -20), (-280, -40)]
-POSITIONS2 = [(280, 40), (280, 20), (280, 0), (280, -20), (280, -40)]
+
 
 
 UP = 90
@@ -13,15 +12,15 @@ LEFT = 180
 
 
 class Paddle:
-    def __init__(self):
+    def __init__(self, positions):
         self.segments = []
-        self.segments_1 = []
-        self.create_paddle(self.segments, POSITIONS1)
-        self.create_paddle(self.segments_1, POSITIONS2)
+        self.create_paddle(self.segments, positions)
+
         self.highest_segment = self.segments[0]
         self.lowest_segment = self.segments[len(self.segments)-1]
 
-    def create_paddle(self,segments, positions):
+
+    def create_paddle(self, segments, positions):
         for position in positions:
             new_segment = Turtle('square')
             new_segment.speed('fastest')
@@ -30,20 +29,28 @@ class Paddle:
             new_segment.color('white')
             segments.append(new_segment)
 
-
     def move_up(self):
         self.highest_segment.seth(UP)
-        for segment in range(len(self.segments)-1, 0, -1):
-            new_y = self.segments[segment - 1].ycor()
-            self.segments[segment].sety(new_y)
+        self.up()
         self.highest_segment.forward(20)
 
     def move_down(self):
         self.lowest_segment.seth(DOWN)
+        self.down()
+        self.lowest_segment.forward(20)
+
+    def up(self):
+        for segment in range(len(self.segments)-1, 0, -1):
+            new_y = self.segments[segment - 1].ycor()
+            self.segments[segment].sety(new_y)
+
+    def down(self):
         for segment in range(0, len(self.segments)-1):
             new_y = self.segments[segment + 1].ycor()
             self.segments[segment].sety(new_y)
-        self.lowest_segment.forward(20)
+
+
+
 
 
 
